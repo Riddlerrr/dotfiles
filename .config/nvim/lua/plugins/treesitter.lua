915e2ec682ad82f03b150d758963a345c075b247
Sergey Andronov <sergey@andronov.me>
2024-11-07 11:07:43 +0100

    nvim: associate jbuilder to ruby

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -18,4 +18,12 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, _)
+      vim.filetype.add({
+        extension = { jbuilder = "ruby" },
+      })
+    end,
+  },
 }
