baf21851c74320dd699dfc0b89eb0ec7e3d43478
Sergey Andronov <sergey@andronov.me>
2024-11-07 13:31:16 +0100

    nvim: remap telescope search with ignores files to c-i

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,24 @@ return {
         local line = action_state.get_current_line()
         LazyVim.pick("find_files", { hidden = true, default_text = line })()
       end
+      local find_files_no_ignore = function()
+        local action_state = require("telescope.actions.state")
+        local line = action_state.get_current_line()
+        LazyVim.pick("find_files", { no_ignore = true, default_text = line })()
+      end
 
-      -- rebind <c-h> to find with hidden files
+      -- rebind <c-h> to find with hidden files and <c-i> to find without ignore filtering
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
-        mappings = { n = { H = find_files_with_hidden }, i = { ["<c-h>"] = find_files_with_hidden } },
+        mappings = {
+          n = {
+            ["<c-h>"] = find_files_with_hidden,
+            ["<c-i>"] = find_files_no_ignore,
+          },
+          i = {
+            ["<c-h>"] = find_files_with_hidden,
+            ["<c-i>"] = find_files_no_ignore,
+          },
+        },
       })
     end,
   },
