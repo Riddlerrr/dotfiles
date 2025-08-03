27a951f2fdc8213181fb523bf43bde8edb584387
Sergey Andronov <sergey@andronov.me>
2025-08-03 12:45:13 +0200

    nvim: extend key bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,7 +5,9 @@ vim.opt.swapfile = false
 
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>x', ':write<CR>:quit<CR>')
 
 vim.cmd(":hi statusline guibg=NONE")
