65c2e74da8334db65c7c85d9f423b6793e4a1513
Sergey Andronov <sergey@andronov.me>
2025-08-03 11:45:59 +0200

    Go to minimal neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.swapfile = false
+
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+
+vim.cmd(":hi statusline guibg=NONE")
